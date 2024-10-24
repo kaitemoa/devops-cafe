8ffa9197dbe297b8b672fab6dc334e9c7d9ed6a0
rintajom <rintajom@arcada.fi>
2024-10-24 13:34:54 +0300

    M: added login function

--- src/main/java/CafeProject/cafe/project/Login.java
+++ src/main/java/CafeProject/cafe/project/Login.java
@@ -1,9 +1,31 @@
 package CafeProject.cafe.project;
+import java.util.Scanner;
 
-public class Login 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Login!" );
+public class Login {
+    public String username = "Spiderman";
+    public String password = "123";
+
+    public boolean login(String userInput, String passInput) {
+        if (this.username.equals(userInput) && this.password.equals(passInput)) {
+            return true; 
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your username: ");
+        String user = in.nextLine();
+        System.out.print("Enter your password: ");
+        String pass = in.nextLine();
+        
+        Login login = new Login();
+        if (login.login(user, pass)) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+        in.close();
     }
 }
